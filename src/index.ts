ed92b5d4a2b1f4a1247ed36b88e8d8e19666ec82
Nguyen Minh Tuan <tnowad@gmail.com>
2023-05-31 17:52:17 +0700

    add bassic setup

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,22 @@
 import express from "express";
+import morgan from "morgan";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:3000/",
+  })
+);
 
-const PORT = process.env.PORT || 3000;
+app.use(morgan("dev"));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const PORT = process.env.PORT ?? 3000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
