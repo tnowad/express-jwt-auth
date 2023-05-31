9ef32466191392a70bce3856d2357bcf2bf562cf
Nguyen Minh Tuan <tnowad@gmail.com>
2023-06-01 01:54:53 +0700

    add async handler

--- src/routes/auth.router.ts
+++ src/routes/auth.router.ts
@@ -1,15 +1,16 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import {
   login,
   logout,
   refreshToken,
   register,
 } from "../controllers/auth.controller";
+import asyncHandler from "express-async-handler";
 const router = express.Router();
 
-router.post("/register", register);
-router.post("/login", login);
-router.post("/refresh-token", refreshToken);
-router.delete("/logout", logout);
+router.post("/register", asyncHandler(register));
+router.post("/login", asyncHandler(login));
+router.post("/refresh-token", asyncHandler(refreshToken));
+router.delete("/logout", asyncHandler(logout));
 
 export default router;
